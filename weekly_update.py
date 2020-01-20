f8cc3ce9f595b7158913bb9c7d8d61e03539a312
codethekitty <mimeaux@gmail.com>
2020-01-20 13:57:56 -0500

    volunteer table

--- weekly_update.py
+++ weekly_update.py
@@ -161,164 +161,30 @@ savefig('shared/figures/lastname_team.png',dpi=300,bbox_inches='tight')
 print('updated run league tables (last name)')
 
 
-#%% volunteer team league table
-
-Dall = df[df.parkrun=='Lillie parkrun, Ann Arbor']
-Dvol = df2[df2.parkrun=='Lillie parkrun, Ann Arbor']
-gs = gridspec.GridSpec(2,1,height_ratios=(1,2))
-
-figure(figsize=(16,8))
-
-firstnames = [x.split(' ') for x in Dvol.volunteer]
-N=[]
-for i in firstnames:
-    if i[0].find('.')>=0:
-        n=i[1]
-    else:
-        n=i[0]
-    N.append(n)
-stat=[]
-for u in unique(N):
-    ix=isin(N,u)
-    cc=len(unique(Dvol[ix].volunteer))
-    if cc>1:
-        Dsub={}
-        for n in unique(Dvol[ix].volunteer):
-            D2=Dvol[ix][Dvol[ix].volunteer==n]            
-            Dsub[n]=len(D2)
-        stat.append({'fname':u,'count':cc,'total_runs':sum(ix),'detail':Dsub})
-stat=pandas.DataFrame.from_dict(stat)
-stat=stat.sort_values(by='total_runs',ascending=False).reset_index().drop(columns='index')
-stat=stat[(stat.total_runs/stat['count'])>1].reset_index().drop(columns='index')
-
-cc=rcParams['axes.prop_cycle'].by_key()['color']
-
-ax = subplot(gs[0])
-xt=[]
-for i,j in stat.iterrows():
-    v=0
-    for k,v1 in j.detail.items():
-        rect = patches.Rectangle((v,len(stat)-i-0.5),v1,1,linewidth=1,edgecolor='w',facecolor=cc[i])
-        if v1>1:
-            ln=k.split(' ')[-1]
-            text(mean([v,v+v1]),len(stat)-i,ln,ha='center',va='center',fontsize=12-1.5*len(ln)/v1,color='w')
-        v+=v1
-        ax.add_patch(rect)
-    xt.append(j.fname+': '+str(j['count']))
-ylim(0,len(stat)+1)
-xlim(0,stat.total_runs[0]+1)
-yticks(arange(len(xt))+1,xt[::-1],ha='right')
-ax.set_xlabel('Total Volunteering',fontsize=12)
-ax.xaxis.set_ticks_position('top')
-ax.xaxis.set_label_position('top') 
-ax.spines['bottom'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ylabel('(by first name)')
-
-fn = datetime.datetime.now().strftime('%Y.%m.%d')
-tt='(updated %s)'%(fn)
-title(tt,fontweight='normal',fontsize=10,loc='right')
-
-firstnames = [x.split(' ') for x in Dvol.volunteer]
-N=[]
-for i in firstnames:
-    n=i[-1]
-    N.append(n)
-stat=[]
-for u in unique(N):
-    ix=isin(N,u)
-    cc=len(unique(Dvol[ix].volunteer))
-    if cc>1:
-        Dsub={}
-        for n in unique(Dvol[ix].volunteer):
-            D2=Dvol[ix][Dvol[ix].volunteer==n]            
-            Dsub[n]=len(D2)
-        stat.append({'fname':u,'count':cc,'total_runs':sum(ix),'detail':Dsub})
-stat=pandas.DataFrame.from_dict(stat)
-stat=stat.sort_values(by='total_runs',ascending=False).reset_index().drop(columns='index')
-stat=stat[(stat.total_runs/stat['count'])>1].reset_index().drop(columns='index')
-
-
-cc=rcParams['axes.prop_cycle'].by_key()['color']
-cc+=cc
-
-ax=subplot(gs[1])
-xt=[]
-for i,j in stat.iterrows():
-    v=0
-    for k,v1 in j.detail.items():
-        rect = patches.Rectangle((v,len(stat)-i-0.5),v1,1,linewidth=1,edgecolor='w',facecolor=cc[i])
-        if v1>1:
-            ln=k.split(' ')[0]
-            if ln=='T.':
-                ln='Charles'
-            text(mean([v,v+v1]),len(stat)-i,ln,ha='center',va='center',fontsize=12-1.5*len(ln)/v1,color='w')
-        v+=v1
-        ax.add_patch(rect)
-    xt.append(j.fname+': '+str(j['count']))
-ylim(0,len(stat)+1)
-xlim(0,stat.total_runs[0]+1)
-yticks(arange(len(xt))+1,xt[::-1],ha='right')
-ax.set_xlabel('Total Volunteering',fontsize=12)
-ax.xaxis.set_ticks_position('top')
-ax.xaxis.set_label_position('top') 
-ax.spines['bottom'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ylabel('(by last name)')
-
-tight_layout()
-
-savefig('shared/figures/name_team_volunteer.png',dpi=300,bbox_inches='tight')
-print('updated volunteer league table (team)')
-
 #%% volunteer individual league table
 
-df3 = df2[df2.parkrun=='Lillie parkrun, Ann Arbor']
-df4 = df[df.parkrun=='Lillie parkrun, Ann Arbor']
-
-cc=rcParams['axes.prop_cycle'].by_key()['color']
-
-dfcount=df3.volunteer.value_counts()
-dfcount=dfcount[dfcount>0]
-xlab=dfcount.index.values
-y2=[]
-for n in xlab:
-    y2.append(sum(df4.parkrunner==n))
-ix=[array(y2)>3]
-y=dfcount.values[ix]
-x=arange(len(y))
-y2=array(y2)[ix]
-xlab=xlab[ix]
-fig,ax=subplots(figsize=[14,16])
-ax.barh(-x,y,facecolor=cc[4])
-ax.barh(-x,-array(y2),facecolor=cc[1])
-ax.set_ylim(min(-x)-1,max(-x)+1)
-ax.set_xticklabels([int(x) for x in abs(ax.get_xticks())])
-ax.set_yticks([])
-ax.spines['left'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['bottom'].set_visible(False)
-for n,xp,right,left in zip(xlab,-x,y,-array(y2)):
-    name = n[:n.find(' ')+2]
-    if name=='T. C':
-        name='T. Charles Y'
-    if name=='Torsten J':
-        name='Torsten Y'
-    if name=='Aldus K':
-        name='Aldus Y'
-    if (right-left)>4:
-        text((right+left)/2,xp,name,ha='center',va='center',color='w',fontsize=10)
+current_count=df2.groupby('parkrun').max().event
+dfv=df2.volunteer.value_counts()
+dfv=dfv[dfv>24]
+table=[]
+for pr,n in dfv.items():
+    homepr = df2[df2.volunteer==pr].parkrun.value_counts().idxmax()
+    rr=sum((df.parkrunner==pr)) #(df.parkrun==homepr)
+    if rr==0:
+        p=inf
     else:
-        text(2.5,xp,name,ha='left',va='center',color='k',fontsize=10)
-text(-30,2.5,'# Runs',ha='center',fontsize=11,color=cc[1],fontweight='bold')
-text(25,2.5,'# Volunteering',ha='center',fontsize=11,color=cc[4],fontweight='bold')
-ax.xaxis.set_ticks_position('top')
-fn = datetime.datetime.now().strftime('%Y.%m.%d')
-tt='(updated %s)'%(fn)
-title(tt,fontweight='normal',fontsize=10,loc='right')
+        p=n/rr
+    pstr='%d/%d'%(n,rr)
+    table.append({'parkrunner':pr,'n_volunteer':n,'home_parkrun':homepr, \
+                  'vol/run':pstr,'ratio':p})
 
-savefig('shared/figures/volunteer_stat1.png',dpi=300,bbox_inches='tight')
-print('updated volunteer league table (individual)')
+table=pandas.DataFrame.from_dict(table).sort_values(by=['ratio','n_volunteer'],ascending=False)
+table=table.iloc[:200,:]
+table['rank']=arange(1,201)
+table=table.drop(['ratio','n_volunteer'],axis=1)
+
+table.to_csv('shared/figures/top200_volunteers.csv',index=False)
+print('updated volunteer')
 
 #%% passionistas
 
